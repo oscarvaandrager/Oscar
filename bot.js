2440d346c5c0119e583f6f3ebbbd9cca6c57b3b7
oscarvaandrager <oscar.vaandrager@gmail.com>
2018-03-26 18:43:24 +0200

    j

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+    // It will listen for messages that will start with `_`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
